3c38fff6d0dbf5bcda2eafca6512ead0f308d2aa
sanguk_han <sanguk_han@tmax.co.kr>
2021-09-23 17:05:11 +0900

    09/23 ~559P

--- src/main/java/com/example/club/config/SecurityConfig.java
+++ src/main/java/com/example/club/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.club.config;
 
+import com.example.club.security.handler.ClubLoginSuccessHandler;
+import com.example.club.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,21 +15,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-@Bean
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
+
+    @Bean
 PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
 }
 
 @Override
 protected void configure(HttpSecurity http) throws Exception{
+/*
     http.authorizeRequests()
             .antMatchers("/sample/all").permitAll()
             .antMatchers("/sample/member").hasRole("USER");
+*/
     http.formLogin();
     http.csrf().disable();
-    http.logout();
-}/*
+    http.oauth2Login().successHandler(successHandler());
+    http.rememberMe().tokenValiditySeconds(60*60*7).userDetailsService(userDetailsService);
+}
+@Bean
+    public ClubLoginSuccessHandler successHandler(){
+    return new ClubLoginSuccessHandler(passwordEncoder());
+
+
+}
+/*
 
 @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/example/club/controller/SampleController.java
+++ src/main/java/com/example/club/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.example.club.controller;
 
 import com.example.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample/")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
         log.info("exAll.................");
@@ -24,6 +26,16 @@ public class SampleController {
         log.info(clubAuthMember);
     }
 
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@zerock.org\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
+        log.info("exMemberOnly.................");
+        log.info("------------------------------");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(){
         log.info("exAdmin.................");
